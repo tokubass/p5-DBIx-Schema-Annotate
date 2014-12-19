6a927b064edaf52d2c4a8f90ddf5bb2c364b0107
tokubass <tokubass@cpan.org>
2014-12-20 02:55:22 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+DBIx::Schema::Annotate - It's new $module
+
+# SYNOPSIS
+
+    use DBIx::Schema::Annotate;
+
+# DESCRIPTION
+
+DBIx::Schema::Annotate is ...
+
+# LICENSE
+
+Copyright (C) tokubass.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+tokubass <tokubass@cpan.org>
---
+++ lib/DBIx/Schema/Annotate.pm
@@ -0,0 +1,39 @@
+package DBIx::Schema::Annotate;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+DBIx::Schema::Annotate - It's new $module
+
+=head1 SYNOPSIS
+
+    use DBIx::Schema::Annotate;
+
+=head1 DESCRIPTION
+
+DBIx::Schema::Annotate is ...
+
+=head1 LICENSE
+
+Copyright (C) tokubass.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tokubass E<lt>tokubass@cpan.orgE<gt>
+
+=cut
+
