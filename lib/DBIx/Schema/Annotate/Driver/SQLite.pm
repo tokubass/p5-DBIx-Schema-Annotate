548e60fc8316bbe18c106ac19c507b520add5b46
tokubass <tokubass@cpan.org>
2014-12-22 18:02:48 +0900

    dump sqlite index

--- lib/DBIx/Schema/Annotate/Driver/SQLite.pm
+++ lib/DBIx/Schema/Annotate/Driver/SQLite.pm
@@ -8,13 +8,20 @@ sub table_ddl {
         my $table_name => 'Str',
     );
 
-    my $sth = $self->{dbh}->prepare(q! SELECT * FROM sqlite_master WHERE type='table' and name = ? !);
-    $sth->execute($table_name);
+    my $schema_row = do {
+        my $sth = $self->{dbh}->prepare(q! SELECT * FROM sqlite_master WHERE type='table' and name = ? !);
+        $sth->execute($table_name);
+        $sth->fetchrow_hashref;
+    };
+    my $index_row = do {
+        my $sth = $self->{dbh}->prepare(q! SELECT * FROM sqlite_master WHERE type='index' and tbl_name = ? !);
+        $sth->execute($table_name);
+        $sth->fetchrow_hashref;
+    };
 
-    my $row = $sth->fetchrow_hashref;
-    return $row->{sql};
+    return $schema_row->{sql} unless $index_row->{sql};
+    return join("\n", $schema_row->{sql}, $index_row->{sql} || '' );
 }
-
 
 1;
 
